88cd4017a298b75bb638aa88b3303e224bbbcdb6
essaiydy <imaneessaiydy2@gmail.com>
2023-10-20 13:25:07 -0700

    t

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,4 +6,25 @@
  * @array: An array
  * @size: Size of array
  */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j = 0;
+	int tmp;
 
+	i = 0;
+	while (i < size)
+	{
+		for (; j < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
