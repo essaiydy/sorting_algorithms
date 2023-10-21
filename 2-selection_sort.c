2fbb28f5ae0385278774b88bf8f7791239465d95
mezoxy <ayoub.mezhar@usmba.ac.ma>
2023-10-21 06:08:36 -0700

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,27 +7,3 @@
  * @size: Size of array
 */
 
-void selection_sort(int *array, size_t size)
-{
-	size_t i, j, k;
-	int min;
-
-	for (i = 0; i < size; i++)
-	{
-		min = array[i];
-		for (j = i; j < size; j++)
-		{
-			if (array[j] < min)
-			{
-				min = array[j];
-				k = j;
-			}
-		}
-		if (min != array[i])
-		{
-			array[k] = array[i];
-			array[i] = min;
-			print_array(array, size);
-		}
-	}
-}
