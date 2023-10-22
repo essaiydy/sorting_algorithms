e39951cde7af5275855687cb3da66e7bc0470798
essaiydy <imaneessaiydy2@gmail.com>
2023-10-21 17:02:06 -0700

    t

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,4 +6,29 @@
  * @array: An array
  * @size: Size of array
 */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, mino;
+	int tmp;
 
+	if(!size || !array)
+		return;
+
+	for (j = 0; j < size - 1; j++)
+	{
+		mino = j;
+		for (i = j + 1; i < size; i++)
+		{
+			if (array[i] < array[mino])
+				mino = i;
+		}
+
+		if (mino != j)
+		{
+			tmp = array[j];
+			array[j] = array[mino];
+			array[mino] = tmp;
+			print_array(array, size);
+		}
+	}
+}
